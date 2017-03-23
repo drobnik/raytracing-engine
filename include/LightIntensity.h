115585fd5ca7347a6854b51f04c8f1707b7d126f
drobnik <karolinadrobnik@gmail.com>
2017-03-23 14:04:43 +0100

    Add light intensity definition.

---
+++ include/LightIntensity.h
@@ -0,0 +1,31 @@
+#ifndef RAYTRACING_ENGINE_LIGHTINTENSITY_H
+#define RAYTRACING_ENGINE_LIGHTINTENSITY_H
+#include <iostream>
+
+class LightIntensity {
+protected:
+    float r;
+    float g;
+    float b;
+public:
+    inline LightIntensity() {r=0.0f, g=0.0f, b=0.0f;};
+    inline LightIntensity(float r1, float g1, float b1) {r=r1, g=g1, b=b1;};
+    inline LightIntensity(float r1, float g1) {r=r1, g=g1, b=0.0f;};
+    inline LightIntensity(float r1) {r=r1, g=0.0f, b=0.0f;};
+
+    float red() {return r;};
+    float green() {return g;};
+    float blue() {return b;};
+
+    LightIntensity operator+(LightIntensity& l);
+    LightIntensity operator-(LightIntensity& l);
+    LightIntensity operator/(LightIntensity& l);
+    bool operator==(LightIntensity& l);
+    friend LightIntensity operator*(float scale, LightIntensity& l);
+    friend LightIntensity operator*(LightIntensity& l, float scale);
+    friend std::ostream operator<<(std::ostream& str, LightIntensity& l);
+    //calc a pixel depending on the intensity
+};
+
+
+#endif //RAYTRACING_ENGINE_LIGHTINTENSITY_H
---
+++ src/LightIntensity.cpp
@@ -0,0 +1,30 @@
+#include "../include/LightIntensity.h"
+
+LightIntensity LightIntensity::operator+(LightIntensity &l) {
+    return LightIntensity(r + l.red(), g + l.green(), b + l.blue());
+}
+
+LightIntensity LightIntensity::operator-(LightIntensity &l) {
+    return LightIntensity(r - l.red(), g - l.green(), b - l.blue());
+}
+
+//do i need this?! vvvv
+LightIntensity LightIntensity::operator/(LightIntensity &l) {
+    return LightIntensity(r/l.red(), g/l.green(), b/l.blue());
+}
+
+bool LightIntensity::operator==(LightIntensity &l) {
+    return (r == l.red() && g == l.green() && b == l.blue());
+}
+
+LightIntensity operator*(float scale, LightIntensity &l) {
+    return LightIntensity(l.red() * scale, l.green() * scale, l.blue() * scale);
+}
+
+LightIntensity operator*(LightIntensity &l, float scale) {
+    return LightIntensity(l.red() * scale, l.green() * scale, l.blue() * scale);
+}
+
+std::ostream operator<<(std::ostream &str, LightIntensity &l) {
+    return str << "Light Intensity [R: " << l.red()<<", G: "<< l.green() << ", B: "<<l.blue();
+}
