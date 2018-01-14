32296b9c97b8ad1b3a9103d3a838fe272d164f0a
drobnik <karolinadrobnik@gmail.com>
2018-01-14 13:37:00 +0000

    Rewrite samplers for cameras.

--- src/Cameras/OrthoCamera.cpp
+++ src/Cameras/OrthoCamera.cpp
@@ -9,65 +9,6 @@ OrthoCamera::OrthoCamera(const Vector3 &e, const Vector3 &look, unsigned int hei
 
 OrthoCamera::OrthoCamera(const OrthoCamera& cam) : Camera(cam){ }
 
-// check it later
-LightIntensity sampler2(int depth, Ray &ray, std::unique_ptr<Tracer> const &tracer, float pixSize) {
-
-    LightIntensity la, lb, lc, ld, le, final;
-    Vector3 direction = Vector3(0.0f, 0.0f, 1.0f);
-    Ray sampleR = ray;
-    Ray ra, rb, rc, rd;
-    ra.setDirection(direction);
-    rb.setDirection(direction);
-    rc.setDirection(direction);
-    rd.setDirection(direction);
-    float offset;
-
-    if(depth >= Camera::ANTI_MAX){
-        final = tracer->rayTrace(ray);
-        return final;
-    }
-    else{
-        offset = pixSize * (float)pow(0.5f, (depth + 1));
-        ra.setOrigin(Vector3(ray.getOrigin().getX() - offset, ray.getOrigin().getY(),
-                             ray.getOrigin().getZ()));
-        rb.setOrigin(Vector3(ray.getOrigin().getX() - offset,
-                             ray.getOrigin().getY()  - offset,
-                             ray.getOrigin().getZ()));
-        rc.setOrigin(Vector3(ray.getOrigin().getX(), ray.getOrigin().getY(),
-                             ray.getOrigin().getZ()));
-        rd.setOrigin(Vector3(ray.getOrigin().getX(), ray.getOrigin().getY() - offset,
-                             ray.getOrigin().getZ()));
-
-        //middle first
-        le = tracer->rayTrace(ray);
-        la = tracer->rayTrace(ra);
-        lb = tracer->rayTrace(rb);
-        lc = tracer->rayTrace(rc);
-        ld = tracer->rayTrace(rd);
-
-        if(le != la){
-            la = sampler2((depth + 1), ray, tracer, pixSize);
-        }
-        else if(le != lb){
-            lb = sampler2((depth + 1), ray, tracer, pixSize);
-        }
-        else if(le != lc){
-            lc = sampler2((depth + 1), ray, tracer, pixSize);
-        }
-        else if(le != ld){
-            ld = sampler2((depth + 1), ray, tracer, pixSize);
-        }
-
-        float finR, finG, finB;
-        finR = (la.Red() + lb.Red() + lc.Red() + ld.Red())/4.0f;
-        finG = (la.Green() + lb.Green() + lc.Green() + ld.Green())/4.0f;
-        finB = (la.Blue() + lb.Blue() + lc.Blue() + ld.Blue())/4.0f;
-
-        final = LightIntensity(finR, finG, finB);
-        return final;
-    }
-}
-
 EngineImage OrthoCamera::RenderScene(LightIntensity &background, std::unique_ptr<Tracer> const &tracer) {
     LightIntensity pixelColor;
     std::string sceneName = tracer->sceneName();
@@ -86,7 +27,7 @@ EngineImage OrthoCamera::RenderScene(LightIntensity &background, std::unique_ptr
             ray.setOrigin(Vector3(x, y, viewDistance));
             ray.setDirection(Vector3(0.0f, 0.0f, -1.0f));
 
-            pixelColor = sampler2(0, ray, tracer, pixelSize);
+            pixelColor = sampler->AdaptiveSampling(ray, tracer, pixelSize, 0, true);
             image.setPixel((int)r, (int)c, pixelColor);
         }
     }
